75f6b91d6ffd084e9928157b4628af178ad34ea8
PetrL45 <petr.larionov.023@pslib.cz>
2025-04-22 13:20:49 +0200

    save2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+const arrow1 = (x:number):number => x + 1
 
-export const App = () => (
-    <p>Ahoj světe</p>
-);
+const arrow2 = (x:number):number => (
+    x + 1
+)
+
+const arrow3 = (x:number):number => { return x + 1 }
+
+export const App = () => {
+    return(
+        <p>Hello World!</p>
+    )
+}
